21f215d19d46390b1bc2a982608e13dfe9806c5b
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-03-01 00:11:04 +0100

    fixed variable checker issues

--- _handle_variables.c
+++ _handle_variables.c
@@ -14,24 +14,24 @@ void _handle_variables(data_t *data)
 	{
 		if (data->argv[i][0] == '$')
 		{
-			/*Get the variable name by removing the '$'*/
-
 			char *var_name = data->argv[i] + 1;
 			char *new_value = NULL;
 
-			/*Handle ?*/
 			if (strcmp(var_name, "?") == 0)
 			{
 				char exit_status_str[10];
-
 				sprintf(exit_status_str, "%d", data->exit_status);
 				new_value = strdup(exit_status_str);
 			}
-			/*Special case for '$'*/
+			else if (strcmp(var_name, "") == 0)
+			{
+				/* Treat standalone $ as a literal $ */
+				new_value = strdup("$");
+			}
 			else if (strcmp(var_name, "$") == 0)
 			{
+				/* Convert PID to string and replace the token */
 				char pid_str[10];
-
 				sprintf(pid_str, "%d", getpid());
 				new_value = strdup(pid_str);
 			}
@@ -39,14 +39,13 @@ void _handle_variables(data_t *data)
 			{
 				char *var_value = getenv(var_name);
 
-				/*Replace token with val if var exists*/
 				if (var_value != NULL)
 				{
 					new_value = strdup(var_value);
 				}
 				else
 				{
-					new_value = _strdup("");
+					new_value = strdup("");
 				}
 			}
 
